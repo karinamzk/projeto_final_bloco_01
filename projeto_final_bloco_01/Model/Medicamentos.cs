1fde699dc1255f89cbae612edb458c0c08a34534
Karina Akina <karinamzk2@gmail.com>
2023-09-11 15:43:13 -0300

    Classe PetShop, Classe Medicamento, Classe Raçoes e Interface IProdutoRepository

---
+++ projeto_final_bloco_01/Model/Medicamentos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_final_bloco_01_.Model
+{
+    internal class Medicamentos
+    {
+    }
+}
---
+++ projeto_final_bloco_01/Repository/IProdutoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_final_bloco_01_.Repository
+{
+    internal interface IProdutoRepository
+    {
+    }
+}
